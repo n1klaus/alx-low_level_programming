77a4892b01dafa370c0f7e15a3d606108aa38df5
Nick Nyanjui <Nicknyanjui@gmail.com>
2022-04-13 15:42:55 -0700

    Update script 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,8 +6,8 @@ int main(void)
 	int putArray[] = {95, 112, 117, 116, 99, 104, 97, 114};
 	for (int i = 0; i < 10; i++ )
 	{
-		_putchar(i);
+		putchar(putArray[i]);
 	}
-	_putchar('\n');
+	putchar('\n');
 	return (0);
 }
