b9273ec1221b9906fd66fd1d4d99ea648105ac4e
Nick Nyanjui <Nicknyanjui@gmail.com>
2022-04-13 15:57:22 -0700

    Add script 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
