295e7ed153f9a7f77796fcf878e9c7b4f25325ec
Nick Nyanjui <Nicknyanjui@gmail.com>
2022-04-13 17:11:15 -0700

    Add script 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints the minutes of a day
+ *
+ * Return: no return
+ */
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = 48; a <= 50; a++)
+	{
+		for (b = 48; b <= 57; b++)
+		{
+			for (c = 48; c <= 53; c++)
+			{
+				for (d = 48; d <= 57; d++)
+				{
+					if (a >= 50 && b >= 52)
+						break;
+					putchar(a);
+					putchar(b);
+					putchar(58);
+					putchar(c);
+					putchar(d);
+					putchar('\n');
+				}
+			}
+		}
+	}
+}
