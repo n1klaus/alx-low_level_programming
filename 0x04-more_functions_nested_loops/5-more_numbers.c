7c2446611e195a81f428b7ceb333cb4fa2d1ad78
Nick Nyanjui <Nicknyanjui@gmail.com>
2022-04-14 14:30:16 -0700

    Add program 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,15 +9,16 @@ void more_numbers(void)
 	int i = 48;
 	int j = 48;
 
-	for (; i <= 57 ; i++)
+	for (; i <= 57; i++)
 	{
 		_putchar(i);
 	}
-	while (j <= 52)
+
+	for (; j <= 52; j++)
 	{
+		i = 49;
 		_putchar(i);
 		_putchar(j);
-		j++;
-	{
+	}
 	_putchar('\n');
 }
