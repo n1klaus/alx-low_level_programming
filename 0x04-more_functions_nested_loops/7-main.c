f877649e81a02abffbe6f48ca38fb7f40c5cfbc8
Nick Nyanjui <Nicknyanjui@gmail.com>
2022-04-14 14:50:31 -0700

    Update program 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-main.c
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -1,4 +1,4 @@
-lude "main.h"
+#include "main.h"
 
 /**
   * main - check the code
--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,4 +1,4 @@
-nclude "holberton.h"
+#include "main.h"
 
 /**
   * print_diagonal - prints diagonal line n times.
