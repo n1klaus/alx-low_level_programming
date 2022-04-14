5a5cf48233a7629776ff2396af0ee6778125e1a6
Nick Nyanjui <Nicknyanjui@gmail.com>
2022-04-14 12:30:00 -0700

    Add program 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * isupper - check the character if uppercase or lower case.
+ * @c: character code for input
+ * Return: Always 0.
+ */
+int isupper(int c)
+{
+	if ((c >= 65) && (c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
