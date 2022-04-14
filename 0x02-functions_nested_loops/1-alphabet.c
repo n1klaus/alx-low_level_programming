4965a5f3866c8b15bc45b818a7c538d8c5c1f4d6
Nick Nyanjui <Nicknyanjui@gmail.com>
2022-04-13 17:23:13 -0700

    Update script 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,10 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
  * print_alphabet - Prints the alphabet in lowercase.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 void print_alphabet(void)
 {
@@ -11,7 +12,7 @@ void print_alphabet(void)
 
 	for (i = 97; i <= 122; i++)
 	{
-		_putchar(i);
+		putchar(i);
 	}
-	_putchar('\n');
+	putchar('\n');
 }
