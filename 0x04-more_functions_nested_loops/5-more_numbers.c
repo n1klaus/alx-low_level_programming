8b678edcb9333476acca48653d84f504dc5e3dea
Nick Nyanjui <Nicknyanjui@gmail.com>
2022-04-14 14:15:54 -0700

    Add program 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * more_numbers - Print numbers from 0 to 9.
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+	int i = 48;
+	int j = 48;
+
+	for (; i <= 57 ; i++)
+	{
+		_putchar(i);
+	}
+	while (j <= 52)
+	{
+		_putchar(i);
+		_putchar(j);
+		j++;
+	{
+	_putchar('\n');
+}
