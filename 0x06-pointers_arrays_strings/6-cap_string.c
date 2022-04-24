2a97f826e50b67eb7f067cbed63f6e7f6fcae6ee
Nick Nyanjui <Nicknyanjui@gmail.com>
2022-04-24 10:49:27 -0700

    Update program 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,9 +9,6 @@ char *cap_string(char *s)
 	int i, count = 0;
 	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	if (*(s + count) >= 97 && *(s + count) <= 122)
-		*(s + count) = *(s + count) - 32;
-	count++;
 	while (*(s + count) != '\0')
 	{
 		for (i = 0; i < 13; i++)
