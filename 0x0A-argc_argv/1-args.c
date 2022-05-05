0ff3aff69d194143b1c047fd71f4c3b820139bb4
Nick Nyanjui <Nicknyanjui@gmail.com>
2022-05-05 13:05:10 -0700

    Add program 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - prints the program's name (first argument in argv),
+ * followed by a new line
+ *
+ * @argc: arguments count
+ * @argv: arguments vector array of strings
+ * Return: number of arguments
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
