ff106e094c3d33992b5cca03692047d749f418c6
Nick Nyanjui <Nicknyanjui@gmail.com>
2022-05-05 13:31:09 -0700

    Add program 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * main - prints the program's name (first argument in argv),
+ * followed by a new line
+ *
+ * @argc: arguments count
+ * @argv: arguments vector array of strings
+ * Return: O if successful
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 3)
+	{
+		printf("%d\n", (atoi(argv[1]) * (atoi(argv[2]))));
+	}
+	else
+	{
+		printf("Error\n"); 
+		return (1);
+	}
+
+	return (0);
+}
