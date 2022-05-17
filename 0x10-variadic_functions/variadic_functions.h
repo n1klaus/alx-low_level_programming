1ac8168c83d39f99aa3cca9cf0dead3e719dddf9
Nick Nyanjui <nicknyanjui@gmail.com>
2022-05-17 12:55:32 -0700

    Add program 1-print_numbers.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int sum_them_all(const unsigned int n, ...);
 int _putchar(char c);
 void print_numbers(const char *separator, const unsigned int n, ...);
