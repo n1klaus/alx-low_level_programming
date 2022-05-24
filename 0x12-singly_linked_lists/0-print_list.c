83ef9b61442f5cce35a01dc126480abcddc6f02a
Nick Nyanjui <nicknyanjui@gmail.com>
2022-05-24 13:27:03 -0700

    Update program 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,10 +7,10 @@
   */
 size_t print_list(const list_t *h)
 {
-	size_t c;
+	unsigned int c;
 
 	c = 0;
-	while (c <= sizeof(*h) && h != NULL)
+	while (h != NULL)
 	{
 		if (h->str != NULL)
 			printf("[%d] %s\n", h->len, h->str);
