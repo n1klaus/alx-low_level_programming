52b0043cb6cc21c43f6ef6850fd524b55616ea87
Nick Nyanjui <nicknyanjui@gmail.com>
2022-05-25 14:00:24 -0700

    Update program 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,6 +6,6 @@
  */
 void __attribute__ ((constructor)) before_main(void)
 {
-	printf("You're beat! and yet, you must allow\n");
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
