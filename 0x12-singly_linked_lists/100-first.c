7b5d5b7e5b3a8e9f2d11e32fc579345e10d07e2a
Nick Nyanjui <nicknyanjui@gmail.com>
2022-05-25 13:58:10 -0700

    Add program 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * before_main - constructor executed before main
+ *
+ * Return: no return.
+ */
+void __attribute__ ((constructor)) before_main(void)
+{
+	printf("You're beat! and yet, you must allow\n");
+	printf("I bore my house upon my back!\n");
+}
