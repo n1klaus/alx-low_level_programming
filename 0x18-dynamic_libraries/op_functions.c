60286ce3334dfdf219fdee70e2ecfc273c7e6249
Nick Nyanjui <nicknyanjui@hotmail.com>
2022-07-11 21:40:16 +0300

    Add 100-operations.so

---
+++ 0x18-dynamic_libraries/op_functions.c
@@ -0,0 +1,54 @@
+/**
+ * add - Adds two numbers
+ * @a: int 1
+ * @b: int 2
+ * Return: Returns result
+**/
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts two numbers
+ * @a: int 1
+ * @b: int 2
+ * Return: Returns result
+**/
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - Multiplies two numbers
+ * @a: int 1
+ * @b: int 2
+ * Return: Returns result
+**/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - Divides two numbers
+ * @a: int 1
+ * @b: int 2
+ * Return: Returns result
+**/
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - Modules two numbers
+ * @a: int 1
+ * @b: int 2
+ * Return: Returns result
+**/
+int mod(int a, int b)
+{
+	return (a % b);
+}
