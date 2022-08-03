d12f08bfd1be1905be61bc3b51aa496a46f744db
Nick Nyanjui <nicknyanjui@gmail.com>
2022-08-03 18:34:48 +0300

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,23 +11,33 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int hashindex = hash_djb2((unsigned char *)key);
 	unsigned long int location = hashindex % ht->size;
+	hash_node_t *temp = NULL;
 
 	if (key == NULL)
-		return(0);
-	if (ht->array[location] != NULL)
+		return (0);
+	temp = ht->array[location];
+	if (temp != NULL)
 	{
 		hash_node_t *new = (hash_node_t *) malloc(sizeof(hash_node_t));
-		if (new == NULL)
-			return(0);
+
+		new->key = (char *) malloc(strlen(key) + 1);
+		new->value = (char *) malloc(strlen(value) + 1);
+		if (new == NULL || new->key == NULL || new->value == NULL)
+			return (0);
 		strcpy(new->key, key);
 		strcpy(new->value, value);
-		ht->array[location]->next = NULL;
-		new->next = ht->array[location];
+		temp->next = NULL;
+		new->next = temp;
+		ht->array[location] = new;
 		return (1);
 	}
-	ht->array[location]->key = (char *) malloc(sizeof(char *));
-	ht->array[location]->value = (char *) malloc(sizeof(char *));
-	strcpy(ht->array[location]->key, key);
-	strcpy(ht->array[location]->value, value);
-	return(1);
+	temp = (hash_node_t *) malloc(sizeof(hash_node_t));
+	temp->key = (char *) malloc(strlen(key) + 1);
+	temp->value = (char *) malloc(strlen(value) + 1);
+	if (temp == NULL || temp->key == NULL || temp->value == NULL)
+		return (0);
+	strcpy(temp->key, key);
+	strcpy(temp->value, value);
+	temp->next = NULL;
+	return (1);
 }
--- 0x1A-hash_tables/3-main.c
+++ 0x1A-hash_tables/3-main.c
@@ -10,9 +10,32 @@
  */
 int main(void)
 {
-    hash_table_t *ht;
+	hash_table_t *ht;
+	int status = 0, counter = 0;
 
-    ht = hash_table_create(1024);
-    hash_table_set(ht, "betty", "cool");
-    return (EXIT_SUCCESS);
+	ht = hash_table_create(1024);
+	status = hash_table_set(ht, "betty", "cool");
+	printf("%d Status :=> %d\n", counter++, status);
+
+	status = hash_table_set(ht, "hetairas", "mentioner");
+	printf("%d Status :=> %d\n", counter++, status);
+
+	status = hash_table_set(ht, "heliotropes", "neurospora");
+	printf("%d Status :=> %d\n", counter++, status);
+
+	status = hash_table_set(ht, "depravement", "serafins");
+	printf("%d Status :=> %d\n", counter++, status);
+
+	status = hash_table_set(ht, "stylist", "subgenera");
+	printf("%d Status :=> %d\n", counter++, status);
+
+	status = hash_table_set(ht, "joyful", "synaphea");
+	printf("%d Status :=> %d\n", counter++, status);
+
+	status = hash_table_set(ht, "redescribed", "urites");
+	printf("%d Status :=> %d\n", counter++, status);
+
+	status = hash_table_set(ht, "dram", "vivency");
+	printf("%d Status :=> %d\n", counter++, status);
+	return (EXIT_SUCCESS);
 }
