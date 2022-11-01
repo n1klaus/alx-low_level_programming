043f64380a54d9ae47e312272e4aecf4c2de3c86
Nick Nyanjui <nicknyanjui@gmail.com>
2022-11-01 19:36:37 +0300

    Update 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,12 +1,12 @@
 #include "search_algos.h"
 /**
  * linear_search - searches for a value in an array of integers
- * array : pointer to the first element of the array to search in
- * size : number of elements in array
- * value : value to search for
+ * @array : pointer to the first element of the array to search in
+ * @size : number of elements in array
+ * @value : value to search for
  *
- * Return : the first index where value is located in array,
- * 	otherwise -1 if not present in array
+ * Return: the first index where value is located in array,
+ *	otherwise -1 if not present in array
  */
 int linear_search(int *array, size_t size, int value)
 {
@@ -23,5 +23,3 @@ int linear_search(int *array, size_t size, int value)
 	}
 	return (-1);
 }
-
-
